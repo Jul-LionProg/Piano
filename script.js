17b560c53d4fc23f83610e5d52ef6dd0bf9d191e
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-03-21 09:06:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -207,3 +207,8 @@ const start = () => {
         input_octave.value = current_octave;
         break;
       }
+    case "z": {
+        if (current_octave > MIN_OCTAVE) current_octave--;
+        input_octave.value = current_octave;
+        break;
+      }
