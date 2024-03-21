2887313a0914d9d34fcacb6f0d6e86ed780e75f2
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-03-21 08:57:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+const NOTE_N_C2 = 16;
+const NOTE_N_C3 = 28;
+const NOTE_N_CS3 = 29;
+const NOTE_N_D3 = 30;
+const NOTE_N_DS3 = 31;
+const NOTE_N_E3 = 32;
+const NOTE_N_F3 = 33;
+const NOTE_N_FS3 = 34;
+const NOTE_N_G3 = 35;
+const NOTE_N_GS3 = 36;
+const NOTE_N_A3 = 37;
+const NOTE_N_AS3 = 38;
+const NOTE_N_B3 = 39;
+const ALL_NOTES = [
